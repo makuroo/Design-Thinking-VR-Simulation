baceaf633e692e4146ffd05a1082de2c293c3c93
makuroo <richardhuang1705@gmail.com>
2023-10-27 15:07:27 +0700

    update

--- Design Thinking Simulation/Assets/Scripts/UserPersonaHistory/UserPersonHistoryUI.cs
+++ Design Thinking Simulation/Assets/Scripts/UserPersonaHistory/UserPersonHistoryUI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UserPersonHistoryUI : MonoBehaviour
 {
+    bool historyCanvasOn = false;
+    bool viewHistoryCanvasOn = false;
     // Start is called before the first frame update
     [SerializeField] Canvas viewHistoryCanvas;
     [SerializeField] Canvas historyCanvas;
@@ -11,12 +13,37 @@ public class UserPersonHistoryUI : MonoBehaviour
     public void ViewHistory()
     {
         viewHistoryCanvas.gameObject.SetActive(false);
+        viewHistoryCanvasOn = false;
         historyCanvas.gameObject.SetActive(true);
+        historyCanvasOn = true;
     }
 
     public void Close()
     {
         viewHistoryCanvas.gameObject.SetActive(true);
+        viewHistoryCanvasOn = true;
         historyCanvas.gameObject.SetActive(false);
+        historyCanvasOn = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (viewHistoryCanvasOn)
+                viewHistoryCanvas.gameObject.SetActive(true);
+
+            if (historyCanvasOn)
+                historyCanvas.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            viewHistoryCanvas.gameObject.SetActive(false);
+            viewHistoryCanvas.gameObject.SetActive(false);
+        }
     }
 }
