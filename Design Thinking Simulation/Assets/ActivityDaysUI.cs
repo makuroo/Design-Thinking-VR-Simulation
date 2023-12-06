5b1d35418ca7db16b0239dd4c7acfe59fd353f36
makuroo <richardhuang1705@gmail.com>
2023-12-06 22:00:28 +0700

    update

--- Design Thinking Simulation/Assets/ActivityDaysUI.cs
+++ Design Thinking Simulation/Assets/ActivityDaysUI.cs
@@ -18,7 +18,7 @@ public class ActivityDaysUI : MonoBehaviour
         }
         else
         {
-            problemStatementTextDays.text = "Menyelesaikan: <color=red>" + GameManager.Instance.interviewCount.ToString() + "</color>/ 3 User Persona";
+            problemStatementTextDays.text = "Menyelesaikan: <color=red>" + GameManager.Instance.interviewCount.ToString() + "</color>/ 3 <b>User Persona</b>";
         }
 
         if (GameManager.Instance.hasDoneProblemStatement)
@@ -27,7 +27,7 @@ public class ActivityDaysUI : MonoBehaviour
         }
         else
         {
-            vpcTextDays.text = "Menyelesaikan 1 Problem Statement";
+            vpcTextDays.text = "Menyelesaikan 1 <b>Problem Statement</b>";
         }
     }
 }
